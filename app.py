c521d94ceb760cc6c1860ce1f6ec7cbbee1de951
Narri95 <110390739+Narri95@users.noreply.github.com>
2026-03-27 22:54:16 +0000

    Add PDF chatbot with Gemini embeddings and conversational chain

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from src.helper import get_pdf_text,get_text_chunks,get_vector_store, get_conversational_chain
+
+def user_input(user_question):
+    response=st.session_state.conversation({"question":user_question})
+    st.session_state.chatHistory=response['chat_history']
+    for i, message in enumerate(st.session_state.chatHistory):
+        if i%2 ==0:
+            st.write("User:", message.content)
+        else:
+            st.write("Reply:" , message.content)    
+def main():
+    st.set_page_config("GENAI_1")
+    st.header("Information-Retrieval-system")
+
+    user_question=st.text_input("Ask a Question from the PDF files")
+
+
+    with st.sidebar:
+        st.title("Menu")
+        pdf_docs=st.file_uploader("upload your  Pdf files and click on the submit&Process button", accept_multiple_files=True)
+        if st.button("Submit &Process"):
+            with st.spinner("Processing..."):
+                raw_text=get_pdf_text(pdf_docs)
+                text_chunks=get_text_chunks(raw_text)
+                vector_store=get_vector_store(text_chunks)
+                st.session_state.conversation=get_conversational_chain(vector_store)
+
+                st.success("Done")
+
+
+if __name__=="__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="GenAI_1",
+    version='0.0.0',
+    author='narri',
+    author_email='narri2011@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+
+)
